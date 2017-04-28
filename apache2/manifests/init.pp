ad7c01593d2d3136d25bb7601cb6b315b1eb48a8
AleLaht <lahtinen.ale@gmail.com>
2017-04-28 14:02:58 +0300

    Remove exec from init.pp

--- apache2/manifests/init.pp
+++ apache2/manifests/init.pp
@@ -1,9 +1,5 @@
 class apache2 {
-	exec {'apt-update':
-		command => '/usr/bin/apt-get update',
-	}
 	package {apache2:
-		require => Exec['apt-update'],
 		ensure => 'installed',
 	}
 	file {'/var/www/html/index.html':
